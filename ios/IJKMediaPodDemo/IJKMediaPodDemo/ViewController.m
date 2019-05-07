10677e7ede71f8c0bfd9b246acbce003660ff3fd
ashoka <savebtc11@gmail.com>
2019-05-07 18:49:44 +0800

    complete pod demo

--- ios/IJKMediaPodDemo/IJKMediaPodDemo/ViewController.m
+++ ios/IJKMediaPodDemo/IJKMediaPodDemo/ViewController.m
@@ -7,9 +7,17 @@
 //
 
 #import "ViewController.h"
-#import <IJKMediaPlayer/IJKMediaPlayer.h>
+#import <IJKMediaFramework/IJKMediaFramework.h>
+#import "ViewControllers/IJKMoviePlayerViewController.h"
+#import "Vendors/Reachability/Reachability.h"
 
-@interface ViewController ()
+const NSString *url = @"http://vfx.mtime.cn/Video/2017/03/31/mp4/170331093811717750.mp4";
+
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray *videos;
+@property (nonatomic, strong) Reachability *reach;
 
 @end
 
@@ -19,9 +27,56 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
-    id<IJKMediaPlayback> playback = [[IJKFFMoviePlayerController alloc] initWithContentURL:nil  withOptions:nil];
+//    id<IJKMediaPlayback> playback = [[IJKFFMoviePlayerController alloc] initWithContentURL:nil  withOptions:nil];
+//
+//    [playback shutdown];
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
+    [self checkNetworks];
+}
 
-    [playback shutdown];
+- (void)checkNetworks {
+    _reach = [Reachability reachabilityWithHostName:@"www.baidu.com"];
+    __weak __typeof__(self) weakSelf = self;
+    _reach.reachableBlock = ^(Reachability *reachability) {
+        [weakSelf fetchMovies];
+    };
+    [_reach startNotifier];
+}
+
+- (void)fetchMovies {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSURL *url = [NSURL URLWithString:@"http://api.m.mtime.cn/PageSubArea/TrailerList.api"];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        if (!data) {
+            return;
+        }
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        NSArray *trailers = dict[@"trailers"];
+        self.videos = trailers;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    });
+}
+
+#pragma mark - UITableViewDataSourse
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _videos.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    NSDictionary *video = _videos[indexPath.row];
+    cell.textLabel.text = video[@"movieName"];
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *video = _videos[indexPath.row];
+    NSString *url = video[@"url"];
+    [IJKVideoViewController presentFromViewController:self withTitle:video[@"movieName"] URL:[NSURL URLWithString:url] completion:^{
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
